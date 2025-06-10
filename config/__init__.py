d92bea7f453632941e26c5748256a617f0de7da9
LEAN-arch <jbautistamsc@gmail.com>
2025-06-09 17:28:04 -0700

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,4 @@
- sentinel_project_root/config/__init__.py
+# sentinel_project_root/config/__init__.py
 # This file makes the 'config' directory a Python package.
 # It exposes the singleton 'settings' instance for easy, clean importing.
 
